d0943482dea1bd980097d986da9798771b1f81c5
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-09-05 20:19:16 +0000

    Get our static files stored on Cloudinary

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://lmvntwct:HKMAbEFy9sz9xn-eebr_5gZOCBhwYEiP@trumpet.db.elephantsql.com/lmvntwct"
 os.environ["SECRET_KEY"] = "welcome1*"
+
+os.environ["CLOUDINARY_URL"] = "cloudinary://556619449728318:66KX6KOViJ1QJkNsViw5z94K7HM@df8bd97ok"
