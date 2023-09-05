7e4ef56a57c6f0c088a74f7fa6015d07f39e5b1f
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-09-05 19:57:22 +0000

    Linked with elephant database and heroku

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://lmvntwct:HKMAbEFy9sz9xn-eebr_5gZOCBhwYEiP@trumpet.db.elephantsql.com/lmvntwct"
+os.environ["SECRET_KEY"] = "welcome1*"
--- raj_restaurant/settings.py
+++ raj_restaurant/settings.py
@@ -9,19 +9,23 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile("env.py"):
+   import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v8*e%-%rzc%1&!f96o7p(3mozez$&j)^!4ona^ea4(1rvv0+&*'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -74,12 +78,16 @@ WSGI_APPLICATION = 'raj_restaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
 
 
 # Password validation
